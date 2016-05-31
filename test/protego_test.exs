890d9cc5df4a6f35e50d577f65f4ffa32321bf7a
pshoukry <pshoukry@gmail.com>
2016-05-31 15:15:14 +0200

    clean test

--- test/protego_test.exs
+++ test/protego_test.exs
@@ -1,11 +1,4 @@
 defmodule ProtegoTest do
   use ExUnit.Case
   doctest Protego
-
-  test "defines the macro protego_for" do
-    assert Keyword.has_key?(Protego.Router.__info__(:macros), :protego_for),
-    "It didn't define the protego_for"
-  end
-
-end
 end
