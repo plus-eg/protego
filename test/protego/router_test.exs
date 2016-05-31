35ab8174a75c146769e2d715200e15aebc89a11a
pshoukry <pshoukry@gmail.com>
2016-05-31 14:51:05 +0200

    reorganize code

---
+++ test/protego/router_test.exs
@@ -0,0 +1,11 @@
+defmodule Protego.RouterTest do
+  use ExUnit.Case
+  doctest Protego
+  require IEx
+
+  test "defines the macro protego_for" do
+    assert Keyword.has_key?(Protego.Router.__info__(:macros), :protego_for),
+    "It didn't define the protego_for"
+  end
+
+end
--- test/protego_test.exs
+++ test/protego_test.exs
@@ -1,8 +1,5 @@
 defmodule ProtegoTest do
   use ExUnit.Case
   doctest Protego
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  require IEx
 end
