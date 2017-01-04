141cea88f4bdb9d299267b7e64291358ba3d49db
Peter <pshoukry@gmail.com>
2017-01-04 12:06:21 +0200

    Bump deps versions

--- config/config.exs
+++ config/config.exs
@@ -27,4 +27,13 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+# import_config "#{Mix.env}.exs" 
+  
+config :guardian, Guardian,
+allowed_algos: ["HS512"], 
+verify_module: Guardian.JWT,
+issuer: "Protego",
+ttl: { 30, :days },
+verify_issuer: true,
+secret_key: "secret_key",
+serializer: Protego.GuardianSerializer
