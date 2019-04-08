24b95740c5b06f3ba12789cd7deee2735099528f
Peter Shoukry <pshoukry@gmail.com>
2019-04-08 11:44:32 +0200

    Upgrade dependencies

--- lib/protego.ex
+++ lib/protego.ex
@@ -1,13 +1,16 @@
 defmodule Protego do
+  @moduledoc """
+  Protego is an authentication library for Elixir/Phoenix. Although adding authentication
+  with guardian and uberauth is simple, the goal is to reduce the boilerplate code
+  that needs to be written.
+
+  """
   def router do
     quote do
       import Protego.Router,  only: [protego_for: 1]
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
