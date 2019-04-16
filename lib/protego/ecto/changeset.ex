17eb909abc6a8b9b88396dbe27fbefd369aa7f26
Peter Shoukry <pshoukry@gmail.com>
2019-04-16 18:04:57 +0200

    Clean function call

--- lib/protego/ecto/changeset.ex
+++ lib/protego/ecto/changeset.ex
@@ -2,9 +2,7 @@ defmodule Protego.Changeset do
 
   def error_list_from(changeset) do
     changeset.errors
-    |> Enum.reduce([], fn attribute, errors ->
-      field_name = elem(attribute, 0)
-      error_message = elem(elem(attribute, 1), 0)
+    |> Enum.reduce([], fn {field_name, {error_message, _}}, errors ->
       ["#{field_name} #{error_message}"] ++ errors
     end)
   end
