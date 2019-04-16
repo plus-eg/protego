8746e0249eaf50035e05d108f1b83455e6377669
Peter Shoukry <pshoukry@gmail.com>
2019-04-16 19:02:34 +0200

    Add basic documentation

--- lib/protego/ecto/changeset.ex
+++ lib/protego/ecto/changeset.ex
@@ -1,5 +1,19 @@
-defmodule Protego.Changeset do
+defmodule Protego.Ecto.Changeset do
+  @moduledoc"""
+  The module adds some helper functions required during registration, authentication of users
+  and authorization of resources.
+  """
 
+  @doc"""
+  Converts the typical changeset nested errors map to a flat list of
+  errors.
+
+  ## Example
+  iex> Protego.Ecto.Changeset.error_list_from(%{errors: [email: {"has already been taken", []}]})
+  ["email has already been taken"]
+
+
+  """
   def error_list_from(changeset) do
     changeset.errors
     |> Enum.reduce([], fn {field_name, {error_message, _}}, errors ->
---
+++ test/protego/ecto/changeset_test.exs
@@ -0,0 +1,4 @@
+defmodule Protego.Ecto.ChangesetTest do
+  use ExUnit.Case, async: true
+  doctest Protego.Ecto.Changeset
+end
